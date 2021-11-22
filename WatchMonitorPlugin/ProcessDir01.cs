abc642f86e5be175b0afbb4f03dff44352273723
tgiqfe <tqji31001@gmail.com>
2021-11-22 09:51:06 +0900

    Update ProcessDir01.

--- WatchMonitorPlugin/ProcessDir01.cs
+++ WatchMonitorPlugin/ProcessDir01.cs
@@ -81,50 +81,26 @@ namespace WatchMonitorPlugin
         private bool RecursiveTree(ref WatchPath watch, Dictionary<string, string> dictionary, string path)
         {
             bool ret = false;
-            var info = new DirectoryInfo(path);
 
             dictionary[$"directory_{_serial}"] = path.Replace(_checkingPath, "\\");
             if (watch == null || this._IsStart)
             {
                 ret = true;
                 watch = new WatchPath(PathType.Directory);
-
                 if (Directory.Exists(path))
                 {
-                    watch.Exists = true;
-                    if (_IsCreationTime ?? false)
-                    {
-                        watch.CreationTime = MonitorTimeStamp.GetDirectoryCreationTime(info, _IsDateOnly ?? false, _IsTimeOnly ?? false);
-                        watch.IsDateOnly = _IsDateOnly;
-                        watch.IsTimeOnly = _IsTimeOnly;
-                    }
-                    if (_IsLastWriteTime ?? false)
-                    {
-                        watch.LastWriteTime = MonitorTimeStamp.GetDirectoryLastWriteTime(info, _IsDateOnly ?? false, _IsTimeOnly ?? false);
-                        watch.IsDateOnly = _IsDateOnly;
-                        watch.IsTimeOnly = _IsTimeOnly;
-                    }
-                    if (_IsLastAccessTime ?? false)
-                    {
-                        watch.LastAccessTime = MonitorTimeStamp.GetDirectoryLastAccessTime(info, _IsDateOnly ?? false, _IsTimeOnly ?? false);
-                        watch.IsDateOnly = _IsDateOnly;
-                        watch.IsTimeOnly = _IsTimeOnly;
-                    }
-                    if (_IsAccess ?? false) { watch.Access = AccessRuleSummary.DirectoryToAccessString(info); }
-                    if (_IsOwner ?? false) { watch.Owner = MonitorSecurity.GetDirectoryOwner(info); }
-                    if (_IsInherited ?? false) { watch.Inherited = MonitorSecurity.GetDirectoryInherited(info); }
-                    if (_IsAttributes ?? false) { watch.Attributes = MonitorAttributes.GetAttributes(path); }
-
-
-
-
-
-                }
-                else
-                {
-
+                    WatchDirectoryCheck_Start(watch, path);
                 }
             }
+            else
+            {
+                if (Directory.Exists(path))
+                {
+                    ret |= WatchDirectoryCheck(watch, dictionary, path);
+                }
+            }
+
+
 
 
 
@@ -132,15 +108,106 @@ namespace WatchMonitorPlugin
             return ret;
         }
 
-        private bool WatchFileCheck()
+        private void WatchDirectoryCheck_Start(WatchPath watch, string path)
         {
-            
-            return false;
+            watch.Exists = true;
+            var info = new DirectoryInfo(path);
+
+            if (_IsCreationTime ?? false)
+            {
+                watch.CreationTime = MonitorTimeStamp.GetDirectoryCreationTime(info, _IsDateOnly ?? false, _IsTimeOnly ?? false);
+                watch.IsDateOnly = _IsDateOnly;
+                watch.IsTimeOnly = _IsTimeOnly;
+            }
+            if (_IsLastWriteTime ?? false)
+            {
+                watch.LastWriteTime = MonitorTimeStamp.GetDirectoryLastWriteTime(info, _IsDateOnly ?? false, _IsTimeOnly ?? false);
+                watch.IsDateOnly = _IsDateOnly;
+                watch.IsTimeOnly = _IsTimeOnly;
+            }
+            if (_IsLastAccessTime ?? false)
+            {
+                watch.LastAccessTime = MonitorTimeStamp.GetDirectoryLastAccessTime(info, _IsDateOnly ?? false, _IsTimeOnly ?? false);
+                watch.IsDateOnly = _IsDateOnly;
+                watch.IsTimeOnly = _IsTimeOnly;
+            }
+            if (_IsAccess ?? false) { watch.Access = AccessRuleSummary.DirectoryToAccessString(info); }
+            if (_IsOwner ?? false) { watch.Owner = MonitorSecurity.GetDirectoryOwner(info); }
+            if (_IsInherited ?? false) { watch.Inherited = MonitorSecurity.GetDirectoryInherited(info); }
+            if (_IsAttributes ?? false) { watch.Attributes = MonitorAttributes.GetAttributes(path); }
+            if (_IsChildCount ?? false) { watch.ChildCount = MonitorChildCount.GetDirectoryChildCount(path); }
+
+            return;
         }
 
-        private bool WatchDirectoryCheck()
+        private void WatchFileCheck_Start(WatchPath watch, string path)
         {
-            return false;
+            watch.Exists = true;
+            var info = new FileInfo(path);
+
+            if (_IsCreationTime ?? false)
+            {
+                watch.CreationTime = MonitorTimeStamp.GetFileCreationTime(info, _IsDateOnly ?? false, _IsTimeOnly ?? false);
+                watch.IsDateOnly = _IsDateOnly;
+                watch.IsTimeOnly = _IsTimeOnly;
+            }
+            if (_IsLastWriteTime ?? false)
+            {
+                watch.LastWriteTime = MonitorTimeStamp.GetFileLastWriteTime(info, _IsDateOnly ?? false, _IsTimeOnly ?? false);
+                watch.IsDateOnly = _IsDateOnly;
+                watch.IsTimeOnly = _IsTimeOnly;
+            }
+            if (_IsLastAccessTime ?? false)
+            {
+                watch.LastAccessTime = MonitorTimeStamp.GetFileLastAccessTime(info, _IsDateOnly ?? false, _IsTimeOnly ?? false);
+                watch.IsDateOnly = _IsDateOnly;
+                watch.IsTimeOnly = _IsTimeOnly;
+            }
+            if (_IsAccess ?? false) { watch.Access = AccessRuleSummary.FileToAccessString(info); }
+            if (_IsOwner ?? false) { watch.Owner = MonitorSecurity.GetFileOwner(info); }
+            if (_IsInherited ?? false) { watch.Inherited = MonitorSecurity.GetFileInherited(info); }
+            if (_IsAttributes ?? false) { watch.Attributes = MonitorAttributes.GetAttributes(path); }
+            if (_IsMD5Hash ?? false) { watch.MD5Hash = MonitorHash.GetMD5Hash(path); }
+            if (_IsSHA256Hash ?? false) { watch.SHA256Hash = MonitorHash.GetSHA256Hash(path); }
+            if (_IsSHA512Hash ?? false) { watch.SHA512Hash = MonitorHash.GetSHA512Hash(path); }
+            if (_IsSize ?? false) { watch.Size = info.Length; }
+        }
+
+        private bool WatchDirectoryCheck(WatchPath watch, Dictionary<string, string> dictionary, string path)
+        {
+            var info = new DirectoryInfo(path);
+
+            bool ret = MonitorExists.WatchDirectory(watch, dictionary, _serial, path);
+            ret |= MonitorTimeStamp.WatchDirectoryCreation(watch, dictionary, _serial, _IsCreationTime, info, _IsDateOnly, _IsTimeOnly);
+            ret |= MonitorTimeStamp.WatchDirectoryLastWrite(watch, dictionary, _serial, _IsLastWriteTime, info, _IsDateOnly, _IsTimeOnly);
+            ret |= MonitorTimeStamp.WatchDirectoryLastAccess(watch, dictionary, _serial, _IsLastAccessTime, info, _IsDateOnly, _IsTimeOnly);
+            ret |= MonitorSecurity.WatchDirectoryAccess(watch, dictionary, _serial, _IsAccess, info);
+            ret |= MonitorSecurity.WatchDirectoryOwner(watch, dictionary, _serial, _IsOwner, info);
+            ret |= MonitorSecurity.WatchDirectoryInherited(watch, dictionary, _serial, _IsInherited, info);
+            ret |= MonitorAttributes.WatchDirectory(watch, dictionary, _serial, _IsAttributes, path);
+            ret |= MonitorChildCount.WatchDirectory(watch, dictionary, _serial, _IsChildCount, path);
+
+            return ret;
+        }
+
+        private bool WatchFileCheck(WatchPath watch, Dictionary<string, string> dictionary, string path)
+        {
+            var info = new FileInfo(path);
+
+            bool ret = MonitorExists.WatchFile(watch, dictionary, _serial, path);
+            ret |= MonitorTimeStamp.WatchFileCreation(watch, dictionary, _serial, _IsCreationTime, info, _IsDateOnly, _IsTimeOnly);
+            ret |= MonitorTimeStamp.WatchFileLastWrite(watch, dictionary, _serial, _IsLastWriteTime, info, _IsDateOnly, _IsTimeOnly);
+            ret |= MonitorTimeStamp.WatchFileLastAccess(watch, dictionary, _serial, _IsLastAccessTime, info, _IsDateOnly, _IsTimeOnly);
+            ret |= MonitorSecurity.WatchFileAccess(watch, dictionary, _serial, _IsAccess, info);
+            ret |= MonitorSecurity.WatchFileOwner(watch, dictionary, _serial, _IsOwner, info);
+            ret |= MonitorSecurity.WatchFileInherited(watch, dictionary, _serial, _IsInherited, info);
+            ret |= MonitorAttributes.WatchFile(watch, dictionary, _serial, _IsAttributes, path);
+            ret |= MonitorHash.WatchFileMD5Hash(watch, dictionary, _serial, _IsMD5Hash, path);
+            ret |= MonitorHash.WatchFileSHA256Hash(watch, dictionary, _serial, _IsSHA256Hash, path);
+            ret |= MonitorHash.WatchFileSHA512Hash(watch, dictionary, _serial, _IsSHA512Hash, path);
+            ret |= MonitorSize.WatchFile(watch, dictionary, _serial, _IsSize, info);
+
+            return ret;
         }
     }
 }
