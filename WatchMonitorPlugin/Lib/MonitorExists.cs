ac81ce647d89bcd78844759bed98f465682b1674
tgiqfe <tqji31001@gmail.com>
2021-11-23 14:14:19 +0900

    Add MonitorRegistryType class.

--- WatchMonitorPlugin/Lib/MonitorExists.cs
+++ WatchMonitorPlugin/Lib/MonitorExists.cs
@@ -62,8 +62,49 @@ namespace WatchMonitorPlugin.Lib
             return ret;
         }
 
+        public static bool WatchRegistryKey(
+            WatchPath watch, Dictionary<string, string> dictionary, int serial, string path)
+        {
+            bool ret = false;
+            if (watch.Exists == null)
+            {
+                ret = true;
+                watch.Exists = GetRegistryKeyExists(path);
+            }
+            else
+            {
+                string pathType = "registry";
+                string checkTarget = "Exists";
+
+                bool ret_bool = GetRegistryKeyExists(path);
+                ret = ret_bool != watch.Exists;
+                dictionary[$"{pathType}_{checkTarget}_{serial}"] = ret ?
+                    $"{watch.Exists} -> {ret_bool}" :
+                    ret_bool.ToString();
+                watch.Exists = ret_bool;
+            }
+            return ret;
+        }
+
         #endregion
         #region Get method
+
+        private static bool GetRegistryKeyExists(string path)
+        {
+            using (RegistryKey regKey = RegistryControl.GetRegistryKey(path, false, false))
+            {
+                return regKey != null;
+            }
+        }
+
+        private static bool GetRegistryValueExists(string path, string name)
+        {
+            using (RegistryKey regKey = RegistryControl.GetRegistryKey(path, false, false))
+            {
+                if (regKey == null) { return false; }
+                return regKey.GetValueNames().Any(x => x.Equals(name, StringComparison.OrdinalIgnoreCase));
+            }
+        }
 
         #endregion
     }
---
+++ WatchMonitorPlugin/Lib/MonitorRegistryType.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace WatchMonitorPlugin.Lib
+{
+    internal class MonitorRegistryType
+    {
+        #region Check method
+
+        public static bool WatchRegistry(
+            WatchPath watch, Dictionary<string, string> dictionary, int serial, bool? isMonitor, RegistryKey regKey, string name)
+        {
+            if ((!isMonitor ?? true) && watch.RegistryType == null) { return false; }
+
+            bool ret = false;
+            if (watch.RegistryType == null)
+            {
+                ret = true;
+                watch.RegistryType = RegistryControl.ValueKindToString(regKey.GetValueKind(name));
+            }
+            else
+            {
+                string pathType = "registry";
+                string checkTarget = "RegistryType";
+
+                string ret_string = RegistryControl.ValueKindToString(regKey.GetValueKind(name));
+                ret = ret_string != watch.RegistryType;
+                dictionary[$"{pathType}_{checkTarget}_{serial}"] = ret ?
+                    $"{watch.RegistryType} -> {ret_string}" :
+                    ret_string;
+
+                watch.RegistryType = ret_string;
+            }
+            return ret;
+        }
+
+        #endregion
+        #region Get method
+
+        #endregion
+    }
+}
