74129694e61ff8d41319e7ea8fd0b42045ba6bce
tgiqfe <tqji31001@gmail.com>
2021-12-10 14:53:38 +0900

    update MonitorExists class.

--- WatchMonitorPlugin/Lib/MonitorExists.cs
+++ WatchMonitorPlugin/Lib/MonitorExists.cs
@@ -10,10 +10,132 @@ using IO.Lib;
 namespace Audit.Lib
 {
     [System.Runtime.Versioning.SupportedOSPlatform("windows")]
-    internal class MonitorExists
+    internal class MonitorExists : MonitorBase
     {
+        public override string CheckTarget { get { return "Exists"; } }
+
+        #region Compare method
+
+        public override bool CompareFile(MonitoringCompare monitoring, Dictionary<string, string> dictionary, int serial)
+        {
+            bool retA = ((FileInfo)monitoring.InfoA).Exists;
+            bool retB = ((FileInfo)monitoring.InfoB).Exists;
+
+            dictionary[$"{monitoring.PathTypeName}A_{this.CheckTarget}_{serial}"] = retA.ToString();
+            dictionary[$"{monitoring.PathTypeName}B_{this.CheckTarget}_{serial}"] = retB.ToString();
+            return retA && retB;
+        }
+
+        public override bool CompareDirectory(MonitoringCompare monitoring, Dictionary<string, string> dictionary, int serial)
+        {
+            bool retA = ((DirectoryInfo)monitoring.InfoA).Exists;
+            bool retB = ((DirectoryInfo)monitoring.InfoB).Exists;
+
+            dictionary[$"{monitoring.PathTypeName}A_{this.CheckTarget}_{serial}"] = retA.ToString();
+            dictionary[$"{monitoring.PathTypeName}B_{this.CheckTarget}_{serial}"] = retB.ToString();
+            return retA && retB;
+        }
+
+        public override bool CompareRegistryKey(MonitoringCompare monitoring, Dictionary<string, string> dictionary, int serial)
+        {
+            bool retA = monitoring.KeyA != null;
+            bool retB = monitoring.KeyB != null;
+
+            dictionary[$"{monitoring.PathTypeName}A_{this.CheckTarget}_{serial}"] = retA.ToString();
+            dictionary[$"{monitoring.PathTypeName}B_{this.CheckTarget}_{serial}"] = retB.ToString();
+            return retA && retB;
+        }
+
+        public override bool CompareRegistryValue(MonitoringCompare monitoring, Dictionary<string, string> dictionary, int serial)
+        {
+            bool retA = monitoring.KeyA?.GetValueNames().Any(x => x.Equals(monitoring.NameA, StringComparison.OrdinalIgnoreCase)) ?? false;
+            bool retB = monitoring.KeyB?.GetValueNames().Any(x => x.Equals(monitoring.NameB, StringComparison.OrdinalIgnoreCase)) ?? false;
+
+            dictionary[$"{monitoring.PathTypeName}A_{this.CheckTarget}_{serial}"] = retA.ToString();
+            dictionary[$"{monitoring.PathTypeName}B_{this.CheckTarget}_{serial}"] = retB.ToString();
+            return retA && retB;
+        }
+
+        #endregion
+        #region Watch method
+
+        public override bool WatchFile(MonitoringWatch monitoring, Dictionary<string, string> dictionary, int serial)
+        {
+            bool ret = false;
+
+            bool result = ((FileInfo)monitoring.Info).Exists;
+            ret = result != monitoring.Exists;
+            if (monitoring.Exists != null)
+            {
+                dictionary[$"{monitoring.PathTypeName}_{this.CheckTarget}_{serial}"] = ret ?
+                    $"{monitoring.Exists} -> {result}" :
+                    result.ToString();
+            }
+            monitoring.Exists = result;
+
+            return ret;
+        }
+
+        public override bool WatchDirectory(MonitoringWatch monitoring, Dictionary<string, string> dictionary, int serial)
+        {
+            bool ret = false;
+
+            bool result = ((DirectoryInfo)monitoring.Info).Exists;
+            ret = result != monitoring.Exists;
+            if (monitoring.Exists != null)
+            {
+                dictionary[$"{monitoring.PathTypeName}_{this.CheckTarget}_{serial}"] = ret ?
+                    $"{monitoring.Exists} -> {result}" :
+                    result.ToString();
+            }
+            monitoring.Exists = result;
+
+            return ret;
+        }
+
+        public override bool WatchRegistryKey(MonitoringWatch monitoring, Dictionary<string, string> dictionary, int serial)
+        {
+            bool ret = false;
+
+            bool ret_bool = monitoring.Key != null;
+            ret = ret_bool != monitoring.Exists;
+            if (monitoring.Exists != null)
+            {
+                dictionary[$"{monitoring.PathTypeName}_{this.CheckTarget}_{serial}"] = ret ?
+                    $"{monitoring.Exists} -> {ret_bool}" :
+                    ret_bool.ToString();
+            }
+            monitoring.Exists = ret_bool;
+
+            return ret;
+        }
+
+        public override bool WatchRegistryValue(MonitoringWatch monitoring, Dictionary<string, string> dictionary, int serial)
+        {
+            bool ret = false;
+
+            bool result = monitoring.Key?.GetValueNames().Any(x => x.Equals(monitoring.Name, StringComparison.OrdinalIgnoreCase)) ?? false;
+            ret = result != monitoring.Exists;
+            if (monitoring.Exists != null)
+            {
+                dictionary[$"{monitoring.PathTypeName}_{this.CheckTarget}_{serial}"] = ret ?
+                    $"{monitoring.Exists} -> {result}" :
+                    result.ToString();
+            }
+            monitoring.Exists = result;
+
+            return ret;
+        }
+
+        #endregion
+
+
+
+
+
         const string CHECK_TARGET = "Exists";
 
+        /*
         #region Compare method
 
         public static bool CompareFile(MonitoringCompare compare, Dictionary<string, string> dictionary, int serial)
@@ -29,12 +151,14 @@ namespace Audit.Lib
 
             return retA && retB;
         }
+        
+         #endregion
+         */
 
 
 
 
 
-        #endregion
         #region Watch method
 
         public static bool WatchFile(
