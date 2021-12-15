1af75c53bc05ad625655afb3f4f0911ef1fcf3e8
tgiqfe <tqji31001@gmail.com>
2021-12-15 21:16:54 +0900

    Test pattern 1.

--- WatchMonitorPlugin/Program.cs
+++ WatchMonitorPlugin/Program.cs
@@ -15,11 +15,11 @@ namespace WatchMonitorPlugin
             Console.WriteLine("Watch対象を指定");
             string targetFile = Console.ReadLine();
 
-            string[] targetFiles = targetFile.Contains(";") ?
+            string[] targets = targetFile.Contains(";") ?
                 targetFile.Split(';').Select(x => x.Trim()).ToArray() :
                 new string[1] { targetFile };
 
-            TestWatchFile(targetFiles);
+            TestWatchDirectory(targets);
 
             Console.ReadLine();
         }
@@ -54,6 +54,68 @@ namespace WatchMonitorPlugin
             }
 
             void checkWatchFile(WatchFile watch)
+            {
+                watch.MainProcess();
+                bool success = watch.Success;
+                Dictionary<string, string> dictionary = watch.Propeties;
+
+                //  確認用
+                foreach (KeyValuePair<string, string> pair in dictionary)
+                {
+                    Console.WriteLine(pair.Key + " : " + pair.Value);
+                }
+
+                if (success)
+                {
+                    Console.Write("[");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Success");
+                    Console.ResetColor();
+                    Console.WriteLine("]");
+                }
+                else
+                {
+                    Console.Write("[");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("Failed");
+                    Console.ResetColor();
+                    Console.WriteLine("]");
+                }
+            }
+        }
+
+        private static void TestWatchDirectory(string[] targetPaths)
+        {
+            WatchDirectory beginWatch = new WatchDirectory()
+            {
+                _ID = ID,
+                _Path = targetPaths,
+                _IsLastWriteTime = true,
+                _IsMD5Hash = true,
+                _IsChildCount = true,
+                _IsAccess = true,
+            };
+            checkWatchFile(beginWatch);
+
+            while (true)
+            {
+                var key = Console.ReadKey(false);
+                switch (key.Key)
+                {
+                    case ConsoleKey.Enter:
+                        var tempWatch = new WatchDirectory()
+                        {
+                            _ID = ID,
+                            _Path = targetPaths,
+                        };
+                        checkWatchFile(tempWatch);
+                        break;
+                    case ConsoleKey.Escape:
+                        return;
+                }
+            }
+
+            void checkWatchFile(WatchDirectory watch)
             {
                 watch.MainProcess();
                 bool success = watch.Success;
